7302a93fbb6300f7af53c00520494951b3236646
mattaptivada <matt@aptivada.com>
2020-10-22 14:43:04 -0600

    added bulma

--- budget/functions.php
+++ budget/functions.php
@@ -29,8 +29,7 @@ function template_header($title) {
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>$title</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/main.css">
     <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
     <script src="js/main.js"></script>
 </head>
