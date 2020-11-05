9cb588f5fc2a45c0956f30442c4df92fd520071c
Aaron Whipple <ohhellwhipple@gmail.com>
2020-11-04 18:43:47 -0700

    added reset password capability

--- budget/forgot.php
+++ budget/forgot.php
@@ -9,8 +9,34 @@ if(isset($_POST["submit"])){
         $msg = 'Email is not valid';
         echo "<script type='text/javascript'>alert('$msg');</script>";
     }
+    //validate account exists, if so send activation code
+    if($sql = $con->prepare('SELECT * FROM users WHERE email = ?')) {
+        $sql->bind_param('s', $_POST['email']);
+        $sql->execute();
+        $sql->store_result();
 
-    //validate account exists, if so resend activation code
+        if ($sql->num_rows > 0) {
+
+            //email content
+            $from = 'support@budgetrite.com';
+            $subject = "Reset your Budget Rite Password";
+            $headers = 'From: ' . $from . "\r\n" . 'Reply-To: ' . $from . "\r\n" . 'X-Mailer: PHP/' . phpversion() . "\r\n" . 'MIME-Version: 1.0' . "\r\n" . 'Content-Type: text/html; charset=UTF-8' . "\r\n";
+            $reset_link = 'http://icarus.cs.weber.edu/~aw54652/web_4350/budget/reset_password.php?email=' . $_POST['email'];
+            $message = '<p>Please click the following link to reset your password: <a href="' . $reset_link. '">Reset Password</a></p>';
+
+
+            mail($_POST['email'],$subject,$message,$headers);
+
+            $msg = 'Please check your email for reset link';
+            echo "<script type='text/javascript'>alert('$msg');</script>";
+            header( "Refresh:1; url=http://icarus.cs.weber.edu/~aw54652/web_4350/budget/index.php");
+
+        } else {
+            //email  does not exists
+            $msg = "No such user exists in our system. Please re-enter correct email or register.";
+            echo "<script type='text/javascript'>alert('$msg');</script>";
+        }
+    }
 }
 ?>
 
@@ -25,7 +51,7 @@ if(isset($_POST["submit"])){
             </h1>
             <form action="forgot.php" method="post">
                 <div class="field">
-                    <label class="label">Enter your email:</label>
+                    <label class="label">Enter your email to send password code:</label>
                     <div class="control">
                         <input class="input" name="email" type="email" placeholder="e.g. johndoe@gmail.com" required>
                     </div>
